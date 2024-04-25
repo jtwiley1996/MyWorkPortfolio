f532a44e57fa62153b9213767a7d6a9db92d27a9
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-04-24 19:34:36 -0500

    fixed links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className='max-w-[1200px] sm:h-[150px] p-12 md:flex md:justify-between mx-auto md:items-center inline-block text-center'>
             <span className='primary-color'>Julian T <br/>Wiley</span>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white mt-3 md:mt-0">
-                <a href="https://www.instagram.com/sthiwar_17/" target="_blank" rel="noreferrer"><AiOutlineInstagram className='contact-shadow'/></a>
-                <a href="https://www.linkedin.com/in/sergio-ardila-alvarado-550b40152/" target="_blank" rel="noreferrer"><AiFillLinkedin className='contact-shadow'/></a>
-                <a href="https://github.com/Stroyer210" target="_blank" rel="noreferrer"><AiFillGithub className='contact-shadow'/></a>
+                <a href="https://www.instagram.com/jayteewiley" target="_blank" rel="noreferrer"><AiOutlineInstagram className='contact-shadow'/></a>
+                <a href="https://www.linkedin.com/in/julian-wiley-51508313b/" target="_blank" rel="noreferrer"><AiFillLinkedin className='contact-shadow'/></a>
+                <a href="https://github.com/jtwiley1996" target="_blank" rel="noreferrer"><AiFillGithub className='contact-shadow'/></a>
             </div>
             <p className='text-gray-600 mt-3 md:mt-0'>jtwiley1996@gmail.com <br/> Let's work together </p>    
         </div>
