1a0480e731ac25366be1a4a4cda5d25f99f4b863
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-04-24 19:29:33 -0500

    added navbar and footer

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import Hero from './components/Hero';
 import About from './components/About';
 import Work from './components/Work';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Hero />,
-      },
-      {
-        path: '/About',
-        element: <About />,
-      },
-      {
-        path: '/Work',
-        element: <Work />,
-      },
-      {
-        path: '/Contact',
-        element: <Contact />,
-      },
-      {
-        path: '/Resume',
-        element: <Resume />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
