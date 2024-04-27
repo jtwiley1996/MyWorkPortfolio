ceb9fbdc2bb913823ac4c30c6501ac80fa48efc7
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-04-26 23:03:44 -0500

    added more work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,7 @@ import project1 from '../assets/project1.png'
 import project2 from '../assets/project2.png'
 import project3 from '../assets/project3.png'
 import project4 from '../assets/project4.png'
+import project5 from '../assets/project5.png'
 import { AiFillGithub } from 'react-icons/ai';
 
 const Work = () => {
@@ -63,7 +64,18 @@ const Work = () => {
                     </div>
                 </div>
             </div>
-
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[300px] bg-cover relative'>
+                <img src={project5} alt="project5"/>
+                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
+                    <span className='text-2xl md:text-3xl font-extrabold text-white tracking-wider'>React Task Manager</span>
+                    <div className='pt-8 text-center flex flex-between'>
+                        <a href="https://react-task-manager-jw.netlify.app/" target="_blank" rel="noreferrer">
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg work-live'>Live</button>
+                        </a>
+                        <a href="https://github.com/jtwiley1996/ReactTaskManager" target="_blank" rel="noreferrer"><AiFillGithub className='text-6xl ml-5 text-white git-shadow '/></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     )}
